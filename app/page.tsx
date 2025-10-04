57f87919ca155863c8e542865dfb47c34b012fc3
DreiDev04 <tacujan.andrei@gmail.com>
2025-10-05 02:09:05 +0800

    added forcaster.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+
+import { sdk } from "@farcaster/miniapp-sdk";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    sdk.actions.ready();
+  }, []);
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello, world!</h1>
